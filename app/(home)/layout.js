5d21b6fdaff6ecab51a5956af91aaa3d45ee1c15
Usama Rahman <95021960+usama-rahman@users.noreply.github.com>
2024-05-17 15:54:27 +0600

    11.4

---
+++ app/(home)/layout.js
@@ -0,0 +1,22 @@
+import Navbar from "@/components/Navbar";
+import { dbConnect } from "@/service/mongo";
+import { Inter } from "next/font/google";
+import "../globals.css";
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+    title: "StaySwift",
+    description: "One Place Stop for Hospitability",
+};
+
+export default async function RootLayout({ children }) {
+    await dbConnect();
+    return (
+        <html lang="en">
+            <body className={inter.className}>
+                <Navbar sideMenu={true} />
+                <main>{children}</main>
+            </body>
+        </html>
+    );
+}
---
+++ database/queries/index.js
@@ -0,0 +1,8 @@
+import { hotelModel } from "@/models/hotel-model";
+import { replaceMongoIdInArray } from "@/utils/data-util";
+
+export async function getAllHotels() {
+  const hotels = await hotelModel.find().lean();
+
+  return replaceMongoIdInArray(hotels);
+}
