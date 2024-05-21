2641780e51e14beaa050f96619eee0ceb8ea3535
Usama Rahman <95021960+usama-rahman@users.noreply.github.com>
2024-05-22 03:33:42 +0600

    fix

--- components/user/booking/PastBooking.jsx
+++ components/user/booking/PastBooking.jsx
@@ -1,25 +1,26 @@
 import BookingCard from "./BookingCard";
 
 const PastBooking = ({ bookings }) => {
-    return (
-        <div className="space-y-4">
-            <h2 className="text-xl font-bold">🕛️ Past Bookings</h2>
-            {bookings &&
-                bookings.length > 0 &&
-                bookings.map((booking) => (
-                    <div
-                        id={booking.id}
-                        className="bg-[#ebf6e9] p-4 rounded-md"
-                    >
-                        <BookingCard
-                            hotelId={booking.hotelId}
-                            checkin={booking.checkin}
-                            checkout={booking.checkout}
-                        />
-                    </div>
-                ))}
-        </div>
-    );
+  return (
+    <div className="space-y-4">
+      <h2 className="text-xl font-bold">🕛️ Past Bookings</h2>
+      {bookings &&
+        bookings.length > 0 &&
+        bookings.map((booking) => (
+          <div
+            key={booking.id}
+            id={booking.id}
+            className="bg-[#ebf6e9] p-4 rounded-md"
+          >
+            <BookingCard
+              hotelId={booking.hotelId}
+              checkin={booking.checkin}
+              checkout={booking.checkout}
+            />
+          </div>
+        ))}
+    </div>
+  );
 };
 
 export default PastBooking;
--- components/user/booking/UpcomingBooking.jsx
+++ components/user/booking/UpcomingBooking.jsx
@@ -1,26 +1,27 @@
 import BookingCard from "./BookingCard";
 
 const UpcomingBooking = ({ bookings }) => {
-    return (
-        <div className="space-y-4">
-            <h2 className="text-xl font-bold">⌛️ Upcomming Bookings</h2>
+  return (
+    <div className="space-y-4">
+      <h2 className="text-xl font-bold">⌛️ Upcomming Bookings</h2>
 
-            {bookings &&
-                bookings.length > 0 &&
-                bookings.map((booking) => (
-                    <div
-                        id={booking.id}
-                        className="bg-[#ebf6e9] p-4 rounded-md"
-                    >
-                        <BookingCard
-                            hotelId={booking.hotelId}
-                            checkin={booking.checkin}
-                            checkout={booking.checkout}
-                        />
-                    </div>
-                ))}
-        </div>
-    );
+      {bookings &&
+        bookings.length > 0 &&
+        bookings.map((booking) => (
+          <div
+            key={booking.id}
+            id={booking.id}
+            className="bg-[#ebf6e9] p-4 rounded-md"
+          >
+            <BookingCard
+              hotelId={booking.hotelId}
+              checkin={booking.checkin}
+              checkout={booking.checkout}
+            />
+          </div>
+        ))}
+    </div>
+  );
 };
 
 export default UpcomingBooking;
